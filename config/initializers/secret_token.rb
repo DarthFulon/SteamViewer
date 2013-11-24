0a73a42afa62811bb6cc9c6b9425eb86bede6883
darthfulon@weburg.me <darthfulon@weburg.me>
2013-11-24 17:19:58 +0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Steam::Application.config.secret_token = '5b18f852207532f433383eb431a1e9613b7439253b7e246b9161abb3a541c9622cc18748e836ace03f98747167f27ed027b686d95080b68e7db56a2e233279eb'
