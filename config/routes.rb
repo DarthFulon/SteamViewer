db18b8eee6b417c98ca0ababdb599e4155e302fb
darthfulon@weburg.me <darthfulon@weburg.me>
2013-11-24 17:33:48 +0500

    branch develop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Steam::Application.routes.draw do
+  get "welcome/index"
+  root :to => 'welcome#index'
+  post 'auth/steam/callback' => 'welcome#auth_callback'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Steam::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
