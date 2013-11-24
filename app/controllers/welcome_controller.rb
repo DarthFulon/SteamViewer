cc25243db3e1c2dc2f870ab7f91b33b741a1599c
darthfulon@weburg.me <darthfulon@weburg.me>
2013-11-24 17:47:58 +0500

    added normal sigh in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,10 @@ class WelcomeController < ApplicationController
     session[:current_user] = { :nickname => auth.info['nickname'],
                                           :image => auth.info['image'],
                                           :uid => auth.uid }
+    url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + ENV["STEAM_WEB_API_KEY"] + "&steamids=" + auth.uid
+    uri = URI(url)
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
     redirect_to root_url
   end
 end
